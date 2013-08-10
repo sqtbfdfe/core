853e5fdb926c869abe11c96839fc52361ec928cb
sqtbfdfe <linyiliuzhen@126.com>
2013-08-10 22:09:18 +0800

    格式化代码

--- src/org/liuz/core/security/springsecurity/cxf/SpringSecurityInInterceptor.java
+++ src/org/liuz/core/security/springsecurity/cxf/SpringSecurityInInterceptor.java
@@ -1,63 +1,59 @@
- package org.liuz.core.security.springsecurity.cxf;
+package org.liuz.core.security.springsecurity.cxf;
 
- import java.util.Iterator;
- import java.util.Vector;
- import javax.servlet.http.HttpServletRequest;
- import org.apache.cxf.interceptor.Fault;
- import org.apache.cxf.message.Message;
- import org.apache.cxf.phase.AbstractPhaseInterceptor;
- import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor;
- import org.apache.ws.security.WSSecurityEngineResult;
- import org.apache.ws.security.WSUsernameTokenPrincipal;
- import org.apache.ws.security.handler.WSHandlerResult;
- import org.springframework.security.core.userdetails.UserDetails;
- import org.springframework.security.core.userdetails.UserDetailsService;
- import org.liuz.core.security.springsecurity.SpringSecurityUtils;
+import java.util.Iterator;
+import java.util.Vector;
+import javax.servlet.http.HttpServletRequest;
 
- public class SpringSecurityInInterceptor extends AbstractPhaseInterceptor<Message>
- {
-   private UserDetailsService userDetailsService;
+import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.AbstractPhaseInterceptor;
+import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor;
+import org.apache.ws.security.WSSecurityEngineResult;
+import org.apache.ws.security.WSUsernameTokenPrincipal;
+import org.apache.ws.security.handler.WSHandlerResult;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.liuz.core.security.springsecurity.SpringSecurityUtils;
 
-   public void setUserDetailsService(UserDetailsService userDetailsService)
-   {
-     this.userDetailsService = userDetailsService;
-   }
+public class SpringSecurityInInterceptor extends AbstractPhaseInterceptor<Message> {
+    private UserDetailsService userDetailsService;
 
-   public SpringSecurityInInterceptor() {
-     super("pre-protocol");
-     addAfter(WSS4JInInterceptor.class.getName());
-   }
+    public void setUserDetailsService(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
-   public void handleMessage(Message message) throws Fault {
-     String userName = getUserNameFromWSS4JResult(message);
-     HttpServletRequest request = (HttpServletRequest)message.get("HTTP.REQUEST");
+    public SpringSecurityInInterceptor() {
+        super("pre-protocol");
+        addAfter(WSS4JInInterceptor.class.getName());
+    }
 
-     UserDetails userDetails = this.userDetailsService.loadUserByUsername(userName);
-     SpringSecurityUtils.saveUserDetailsToContext(userDetails, request);
-   }
+    public void handleMessage(Message message) throws Fault {
+        String userName = getUserNameFromWSS4JResult(message);
+        HttpServletRequest request = (HttpServletRequest) message.get("HTTP.REQUEST");
 
-   private String getUserNameFromWSS4JResult(Message message)
-   {
-     Vector results = (Vector)message.getContextualProperty("RECV_RESULTS");
-     if ((results != null) && (!results.isEmpty()))
-     {
-       Iterator localIterator2;
-       for (Iterator localIterator1 = results.iterator(); localIterator1.hasNext();
-         localIterator2.hasNext())
-       {
-         WSHandlerResult result = (WSHandlerResult)localIterator1.next();
-         localIterator2 = result.getResults().iterator();
-         WSSecurityEngineResult securityResult = (WSSecurityEngineResult)localIterator2.next();
-         int action = ((Integer)securityResult.get("action")).intValue();
-         if ((action & 0x1) > 0) {
-           WSUsernameTokenPrincipal token =
-             (WSUsernameTokenPrincipal)securityResult
-             .get("principal");
-           return token.getName();
-         }
-       }
-     }
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(userName);
+        SpringSecurityUtils.saveUserDetailsToContext(userDetails, request);
+    }
 
-     return null;
-   }
- }
+    private String getUserNameFromWSS4JResult(Message message) {
+        Vector results = (Vector) message.getContextualProperty("RECV_RESULTS");
+        if ((results != null) && (!results.isEmpty())) {
+            Iterator localIterator2;
+            for (Iterator localIterator1 = results.iterator(); localIterator1.hasNext();
+                 localIterator2.hasNext()) {
+                WSHandlerResult result = (WSHandlerResult) localIterator1.next();
+                localIterator2 = result.getResults().iterator();
+                WSSecurityEngineResult securityResult = (WSSecurityEngineResult) localIterator2.next();
+                int action = ((Integer) securityResult.get("action")).intValue();
+                if ((action & 0x1) > 0) {
+                    WSUsernameTokenPrincipal token =
+                            (WSUsernameTokenPrincipal) securityResult
+                                    .get("principal");
+                    return token.getName();
+                }
+            }
+        }
+
+        return null;
+    }
+}
